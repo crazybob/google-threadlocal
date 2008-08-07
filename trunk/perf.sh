0b7f4a4b15bdbf52e244783ff115f0aa0ece207a
crazyboblee <crazyboblee@da60fd6c-d353-0410-9ca4-b9fdb003f7f6>
2008-08-07 01:28:56 +0000

    Recorded current performance results.

--- trunk/perf.sh
+++ trunk/perf.sh
@@ -7,12 +7,12 @@ echo ==========================================
 JAVA='java -Xms128m -Xmx128m -server'
 
 echo
-echo Sun\'s ThreadLocal:
+echo Default ThreadLocal:
 echo
 $JAVA -cp out/test TLTest${1}
 
 echo
-echo crazybob\'s ThreadLocal:
+echo Google ThreadLocal:
 echo
 $JAVA -Xbootclasspath/p:out/main -cp out/test TLTest${1}
 
